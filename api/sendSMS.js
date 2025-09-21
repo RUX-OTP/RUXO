ccc6383ef2bfd9b5d2c9d961d00ccbf1cac4518d
RUX-OTP <w4025350@gmail.com>
2025-09-22 00:30:16 +0100

    Update sendSMS.js

--- api/sendSMS.js
+++ api/sendSMS.js
@@ -3,12 +3,12 @@ import Twilio from "twilio";
 export default async function handler(req, res) {
   const { to, message } = req.body;
 
-  const client = new Twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
+  const client = new Twilio(process.env.AC41269f9f2ab722bb198842c3b185a66d, process.env.4a43cf400244ca0eea4cca4fe5b9bfd8);
 
   try {
     const response = await client.messages.create({
       body: message,
-      from: process.env.TWILIO_NUMBER,
+      from: process.env.+14026215872,
       to: to
     });
     res.status(200).json({ success: true, sid: response.sid });
@@ -16,4 +16,3 @@ export default async function handler(req, res) {
     res.status(500).json({ success: false, error: err.message });
   }
 }
-
